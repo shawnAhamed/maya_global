677a7622c8859558fd2807bff750e5d1d387450e
shawnAhamed <shawnjewel@gmail.com>
2020-05-06 03:18:53 +0600

    maya favicon

--- resources/views/en/index.blade.php
+++ resources/views/en/index.blade.php
@@ -8,7 +8,7 @@
     <link rel="stylesheet" href="../css/style.css">
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;500;600;700;800&display=swap" rel="stylesheet">
     <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
-    <title>Maya || Wellbeing Assistant</title>
+    <title>Maya | Everyone’s well-being assistant</title>
 
 </head>
 <body style="font-family: 'Montserrat', sans-serif;">
